312bc2b0e782485dfab9edcfa2db691d4bf59503
Matt Collins <matt@strikesandgutters.com>
2015-07-04 11:00:47 +0100

    tidy up

--- gopher.js
+++ gopher.js
@@ -4,8 +4,12 @@ var config = require('./lib/config.json');
 // bot lib
 var Bot = require('./lib/bot.js');
 
+// my bots
 var bots = {
   gopher: {
     bot: new Bot(config.gopher)
+  },
+  ant: {
+    bot: new Bot(config.ant)
   }
 }

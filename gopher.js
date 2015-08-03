8554a46067e7411865ad02a95f223c89e4f176d4
Matt Collins <matt@strikesandgutters.com>
2015-08-03 21:14:12 +0100

    re-structure

--- gopher.js
+++ gopher.js
@@ -1,13 +1,8 @@
-// config
-var config = require('./lib/config.json');
-
 // bot lib and event handler
 var Bot = require('./lib/bot.js');
-var EventHandler = require('./lib/event_handler.js')();
 
-// my bots
-var bots = {
-  gopher: {
-    bot: new Bot(config.gopher, EventHandler)
-  }
+module.exports = function(token, opts) {
+  
+  return new Bot(token, opts)
+
 }
